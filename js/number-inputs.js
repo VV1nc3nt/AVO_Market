bdad22745c9d9fd9ef805de514a5ba8558ab5ea7
VV1nc3nt <gormmyz@gmail.com>
2022-02-02 16:38:20 +0300

    working on catalog page

---
+++ js/number-inputs.js
@@ -0,0 +1,5 @@
+$(".input_number").keypress(function(event){
+  event = event || window.event;
+  if (event.charCode && event.charCode!=0 && event.charCode!=46 && (event.charCode < 48 || event.charCode > 57) )
+    return false;
+});
