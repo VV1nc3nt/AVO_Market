3285b570266ac1b036762a6f6bbc0b76e8875fae
VV1nc3nt <gormmyz@gmail.com>
2022-02-02 14:33:23 +0300

    finished main slider

--- js/slick-config.js
+++ js/slick-config.js
@@ -2,21 +2,35 @@ $(document).ready(function(){
   $('.main_banner_slider').slick({
     dots: true,
     autoplay: true,
-    autoplaySpeed: 5000
-  });
-  $('.card_container_slider').slick({
-    slidesToShow: 5,
-    appendArrows: $(".btn_arrows"),
+    autoplaySpeed: 5000,
     pauseOnHover: true,
     pauseOnDotsHover: true
   });
+  $('.popular_container_slider').slick({
+    slidesToShow: 5,
+    appendArrows: $(".btn_arrows")
+  });
+  $('.new_container_slider').slick({
+    slidesToShow: 5,
+    appendArrows: $(".btn_arrows_new")
+  });
+  $('.watched_container_slider').slick({
+    slidesToShow: 5,
+    appendArrows: $(".btn_arrows_watched")
+  });
   $('.card_img_slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
     arrows: false,
     dots: true,
-    draggable: false,
-    // fade: true,
+    draggable: false
+  });
+  $('.popular_slider').slick({
+    slidesToShow: 7,
+    appendArrows: $(".btn_arrows_popular")
+  });
+  $('.brands_slider').slick({
+    slidesToShow: 6,
+    appendArrows: $(".btn_arrows_brands"),
+    rows: 2
   });
 });
 	
